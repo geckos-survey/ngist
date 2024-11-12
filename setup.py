f38bc9ae5f742b7a4109447e3a3c716ec893a518
Amelia Fraser-McKelvie <40415417+ameliafm612@users.noreply.github.com>
2024-11-12 11:50:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ def readme():
         return(file.read())
 
 def versionNumber():
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gistPipeline/_version.py')) as versionFile:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ngistPipeline/_version.py')) as versionFile:
         return(versionFile.readlines()[-1].split()[-1].strip("\"'"))
 
-setup(name='gistPipeline',
+setup(name='ngistPipeline',
       version=versionNumber(),
-      description='The GIST Framework: A multi-purpose tool for the analysis and visualisation of (integral-field) spectroscopic data',
+      description='The nGIST Pipeline: An updated multi-purpose tool for the analysis and visualisation of (integral-field) spectroscopic data',
       long_description_content_type="text/markdown",
       long_description=readme(),
       classifiers=[
@@ -20,39 +20,41 @@ setup(name='gistPipeline',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Programming Language :: Python :: 3',
         'Development Status :: 5 - Production/Stable',
-        'License :: Other/Proprietary License',
+        'License :: MIT',
       ],
-      url='https://abittner.gitlab.io/thegistpipeline/',
-      author='Adrian Bittner',
-      author_email='adrian.bittner@eso.org',
-      license='Other/Proprietary License',
+      url='https://geckos-survey.github.io/gist-documentation/',
+      author='Amelia Fraser-McKelvie & Jesse van de Sande',
+      author_email='a.fraser-mckelvie@eso.org',
+      license='MIT',
       packages=find_packages(),
       install_requires=[
-        'astropy>=3.1',
-        'emcee>=2.2',
-        'matplotlib>=3.1',
-        'numpy>=1.17',
-#        'PyQt6>=5.10',
-        'scipy>=1.3',
-        'pyyaml>=5.3',
-        'vorbin>=3.1',
-        'ppxf>=6.7',
-        'plotbin>=3.1',
-        'printStatus>=1.0',
-        'multiprocess>=0.5'
+        'astropy',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'spectral-cube',
+        'extinction',
+        'ipython',
+        'jupyter',
+        'h5py',
+        'joblib',
+        'tqdm',
+        'pip',
+        'ppxf',
+        'vorbin',
+        'plotbin',
       ],
       python_requires='>=3.6',
       entry_points={
         'console_scripts': [
-            'gistPipeline        = gistPipeline.MainPipeline:main'
+            'ngistPipeline        = ngistPipeline.MainPipeline:main'
             ],
         'gui_scripts': [
-            'Mapviewer           = gistPipeline.Mapviewer:main',
-            'gistPlot_kin        = gistPipeline.plotting.gistPlot_kin:main',
-            'gistPlot_lambdar    = gistPipeline.plotting.gistPlot_lambdar:main',
-            'gistPlot_gas        = gistPipeline.plotting.gistPlot_gas:main',
-            'gistPlot_sfh        = gistPipeline.plotting.gistPlot_sfh:main',
-            'gistPlot_ls         = gistPipeline.plotting.gistPlot_ls:main'
+            'Mapviewer           = ngistPipeline.Mapviewer:main',
+            'gistPlot_kin        = ngistPipeline.plotting.gistPlot_kin:main',
+            'gistPlot_gas        = ngistPipeline.plotting.gistPlot_gas:main',
+            'gistPlot_sfh        = ngistPipeline.plotting.gistPlot_sfh:main',
+            'gistPlot_ls         = ngistPipeline.plotting.gistPlot_ls:main'
          ],
       },
       include_package_data=True,
